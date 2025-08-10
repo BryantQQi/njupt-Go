8ef1f92d66fd905adaaa35d318918394bc1ff2f0
Monkey <Monkey@163.com>
2025-08-10 22:58:16 +0800

    feat: 添加日志记录功能 - 在ServiceProductApplication中引入Slf4j并记录服务启动信息

--- service/service-product/src/main/java/com/atnjupt/ServiceProductApplication.java
+++ service/service-product/src/main/java/com/atnjupt/ServiceProductApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-
+import lombok.extern.slf4j.Slf4j;
 /**
  * ClassName:${NAME}
  * Package: com.atnjupt
@@ -14,11 +14,13 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
  * @Create 2025/7/18 15:36
  * @Version 1.0
  */
+@Slf4j
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableFeignClients
 public class ServiceProductApplication {
     public static void main(String[] args) {
         SpringApplication.run(ServiceProductApplication.class,args);
+        log.info("service-product启动成功");
     }
 }
